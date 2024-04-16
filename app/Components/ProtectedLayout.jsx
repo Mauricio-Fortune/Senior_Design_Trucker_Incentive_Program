135d076d5077356292097f750fd4857418fdd872
Ben Jablonski <bvjablo@clemson.edu>
2024-04-16 14:31:35 -0400

    login audit logs

--- app/Components/ProtectedLayout.jsx
+++ app/Components/ProtectedLayout.jsx
@@ -1,262 +1,295 @@
-import {React} from 'react';
+import { React } from 'react';
 import { Authenticator, useAuthenticator, useTheme, View, Heading, Text, Button, Image } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import Layout from './Layout';
-  
+import { Hub } from 'aws-amplify/utils';
+
 const components = {
+  Header() {
+    const { tokens } = useTheme();
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Amplify logo"
+          src="https://docs.amplify.aws/assets/logo-dark.svg"
+        />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Sign in to your account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toForgotPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toForgotPassword}
+            size="small"
+            variation="link"
+          >
+            Reset Password
+          </Button>
+        </View>
+      );
+    },
+  },
+
+  SignUp: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Create a new account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toSignIn}
+            size="small"
+            variation="link"
+          >
+            Back to Sign In
+          </Button>
+        </View>
+      );
+    },
+  },
+  ConfirmSignUp: {
     Header() {
       const { tokens } = useTheme();
       return (
-        <View textAlign="center" padding={tokens.space.large}>
-          <Image
-            alt="Amplify logo"
-            src="https://docs.amplify.aws/assets/logo-dark.svg"
-          />
-        </View>
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
       );
     },
-  
     Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  SetupTotp: {
+    Header() {
       const { tokens } = useTheme();
-  
       return (
-        <View textAlign="center" padding={tokens.space.large}>
-          <Text color={tokens.colors.neutral[80]}>
-            &copy; All Rights Reserved
-          </Text>
-        </View>
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
       );
     },
-  
-    SignIn: {
-      Header() {
-        const { tokens } = useTheme();
-  
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Sign in to your account
-          </Heading>
-        );
-      },
-      Footer() {
-        const { toForgotPassword } = useAuthenticator();
-  
-        return (
-          <View textAlign="center">
-            <Button
-              fontWeight="normal"
-              onClick={toForgotPassword}
-              size="small"
-              variation="link"
-            >
-              Reset Password
-            </Button>
-          </View>
-        );
-      },
+    Footer() {
+      return <Text>Footer Information</Text>;
     },
-  
-    SignUp: {
-      Header() {
-        const { tokens } = useTheme();
-  
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Create a new account
-          </Heading>
-        );
-      },
-      Footer() {
-        const { toSignIn } = useAuthenticator();
-  
-        return (
-          <View textAlign="center">
-            <Button
-              fontWeight="normal"
-              onClick={toSignIn}
-              size="small"
-              variation="link"
-            >
-              Back to Sign In
-            </Button>
-          </View>
-        );
-      },
+  },
+  ConfirmSignIn: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
     },
-    ConfirmSignUp: {
-      Header() {
-        const { tokens } = useTheme();
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Enter Information:
-          </Heading>
-        );
-      },
-      Footer() {
-        return <Text>Footer Information</Text>;
-      },
+    Footer() {
+      return <Text>Footer Information</Text>;
     },
-    SetupTotp: {
-      Header() {
-        const { tokens } = useTheme();
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Enter Information:
-          </Heading>
-        );
-      },
-      Footer() {
-        return <Text>Footer Information</Text>;
-      },
+  },
+  ForgotPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
     },
-    ConfirmSignIn: {
-      Header() {
-        const { tokens } = useTheme();
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Enter Information:
-          </Heading>
-        );
-      },
-      Footer() {
-        return <Text>Footer Information</Text>;
-      },
+    Footer() {
+      return <Text>Footer Information</Text>;
     },
-    ForgotPassword: {
-      Header() {
-        const { tokens } = useTheme();
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Enter Information:
-          </Heading>
-        );
-      },
-      Footer() {
-        return <Text>Footer Information</Text>;
-      },
+  },
+  ConfirmResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
     },
-    ConfirmResetPassword: {
-      Header() {
-        const { tokens } = useTheme();
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Enter Information:
-          </Heading>
-        );
-      },
-      Footer() {
-        return <Text>Footer Information</Text>;
-      },
+    Footer() {
+      return <Text>Footer Information</Text>;
     },
-  };
-  
-  const formFields = {
-    signIn: {
-      username: {
-        placeholder: 'Enter your email',
-        label: 'Email',
-      },
+  },
+};
+
+const formFields = {
+  signIn: {
+    username: {
+      placeholder: 'Enter your email',
+      label: 'Email',
     },
-    signUp: {
-        name: {
-            isRequired: true,
-            order: 2
-        },
-      password: {
-        label: 'Password:',
-        placeholder: 'Enter your Password:',
-        isRequired: false,
-        order: 5,
-      },
-      username: {
-        label: 'Email',
-        placeholder: 'Enter your Email:',
-        isRequired: true,
-        order: 1,
-      },
-      confirm_password: {
-        label: 'Confirm Password:',
-        order: 6,
-      },
-      'custom:user_type': {
-        label: 'Account Type',
-        placeholder: 'Enter Account Type',
-        order: 3
-      },
-      birthdate: {
-        order: 4
-      }
+  },
+  signUp: {
+    name: {
+      isRequired: true,
+      order: 2
     },
-    forceNewPassword: {
-      password: {
-        placeholder: 'Enter your Password:',
-      },
+    password: {
+      label: 'Password:',
+      placeholder: 'Enter your Password:',
+      isRequired: false,
+      order: 5,
     },
-    forgotPassword: {
-      username: {
-        placeholder: 'Enter your email:',
-      },
+    username: {
+      label: 'Email',
+      placeholder: 'Enter your Email:',
+      isRequired: true,
+      order: 1,
     },
-    confirmResetPassword: {
-      confirmation_code: {
-        placeholder: 'Enter your Confirmation Code:',
-        label: 'New Label',
-        isRequired: false,
-      },
-      confirm_password: {
-        placeholder: 'Enter your Password Please:',
-      },
+    confirm_password: {
+      label: 'Confirm Password:',
+      order: 6,
     },
-    setupTotp: {
-      QR: {
-        totpIssuer: 'test issuer',
-        totpUsername: 'amplify_qr_test_user',
-      },
-      confirmation_code: {
-        label: 'New Label',
-        placeholder: 'Enter your Confirmation Code:',
-        isRequired: false,
-      },
+    'custom:user_type': {
+      label: 'Account Type',
+      placeholder: 'Enter Account Type',
+      order: 3
     },
-    confirmSignIn: {
-      confirmation_code: {
-        label: 'New Label',
-        placeholder: 'Enter your Confirmation Code:',
-        isRequired: false,
-      },
+    birthdate: {
+      order: 4
+    }
+  },
+  forceNewPassword: {
+    password: {
+      placeholder: 'Enter your Password:',
     },
-  };
+  },
+  forgotPassword: {
+    username: {
+      placeholder: 'Enter your email:',
+    },
+  },
+  confirmResetPassword: {
+    confirmation_code: {
+      placeholder: 'Enter your Confirmation Code:',
+      label: 'New Label',
+      isRequired: false,
+    },
+    confirm_password: {
+      placeholder: 'Enter your Password Please:',
+    },
+  },
+  setupTotp: {
+    QR: {
+      totpIssuer: 'test issuer',
+      totpUsername: 'amplify_qr_test_user',
+    },
+    confirmation_code: {
+      label: 'New Label',
+      placeholder: 'Enter your Confirmation Code:',
+      isRequired: false,
+    },
+  },
+  confirmSignIn: {
+    confirmation_code: {
+      label: 'New Label',
+      placeholder: 'Enter your Confirmation Code:',
+      isRequired: false,
+    },
+  },
+};
 
 
 const ProtectedLayout = ({ children, level }) => {
-      return (
-        <Authenticator components={components} formFields={formFields}>
-          <Layout>
-          {children}
-          </Layout>
-        </Authenticator>
-      );
+  return (
+    <Authenticator components={components} formFields={formFields}>
+      <Layout>
+        {children}
+      </Layout>
+    </Authenticator>
+  );
 };
+
+const listener = (data) => {
+  if (data.payload.event === 'signedIn') {
+    console.log('Successful Sign In -> Audit Log For: ' + data.payload.data.signInDetails.loginId);
+    async function signInSuccess() {
+      try {
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            user_ID: data.payload.data.userId,
+            status: 'SUCCESS'
+          })
+        };
+        const response = await fetch(`/api/user/login_attempts`, requestOptions);
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const result = await response.json();
+
+      } catch (error) {
+        console.error('Failed to log sign in:', error);
+      }
+    }
+    signInSuccess();
+  };
+}
+
+Hub.listen('auth', listener);
 
 export default ProtectedLayout;
