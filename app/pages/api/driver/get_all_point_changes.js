cb06eb0713f6ca75ca4ef5f6f4ec00194ea305ad
Ethan Ellis <eellis2@clemson.edu>
2024-04-18 18:25:40 -0400

    Point Change Tracking

--- app/pages/api/driver/get_all_point_changes.js
+++ app/pages/api/driver/get_all_point_changes.js
@@ -1,9 +1,12 @@
+// GET driver points
+
 import mysql from 'mysql2/promise';
 import { config } from 'dotenv';
 
 config(); // This loads the .env variables
 
 export default async function handler(req, res) {
+
     // Database connection configuration
     const dbConfig = {
         host: process.env.DB_HOST,
@@ -13,19 +16,24 @@ export default async function handler(req, res) {
         database: process.env.DB_NAME
     };
 
+    
+
     try {
+        // Create a connection to the database
         const connection = await mysql.createConnection(dbConfig);
 
-        const org_ID = req.query.org_ID;
+        //const {user_ID} = req.body;
         const user_ID = req.query.user_ID;
+        const org_ID = req.query.org_ID;
 
-        const row = await connection.query('UPDATE User_Org SET Is_current = 0 WHERE org_ID != ? AND user_ID = ?', [org_ID, user_ID]);
-        const row2 = await connection.query('UPDATE User_Org SET Is_current = 1 WHERE org_ID = ? AND user_ID = ?', [org_ID, user_ID]);
-
+        const [rows] = await connection.query(
+            'SELECT point_change_value, reason, timestamp FROM Point_Changes_Audit WHERE user_ID = ? AND org_ID = ? ORDER BY timestamp DESC', 
+            [user_ID, org_ID]
+        );
+        // Close the database connection
         await connection.end();
 
-        res.status(200).json({"Message": 'Successfully set current org'});
-
+        res.status(200).json({rows});
     } catch (error) {
         console.error('Database connection or query failed', error);
         res.status(500).json({ message: 'Internal Server Error' });
